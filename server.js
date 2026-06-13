ba3d4949d980f9cf8a301343c1b03cc0723a67f1
Gustavo <duartegustavo2007@gmail.com>
2026-06-12 21:24:45 -0300

    estilizando o css

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.get('/',(req,res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em https://localhost:${PORT}`)
+    console.log(`Servidor rodando em https://localhost:${PORT}`);
 });
